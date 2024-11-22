51d546e5a8755a44b5ef004d8b7e16f5ac9857ea
BrunoPasquetti <134561803+BrunoPasquetti@users.noreply.github.com>
2024-11-22 12:43:35 -0300

    Add files via upload

---
+++ api-gateway-service/src/main/java/br/edu/atitus/paradigma/api_gateway_service/configs/ApiGatewayConfig.java
@@ -0,0 +1,37 @@
+package br.edu.atitus.paradigma.api_gateway_service.configs;
+
+import org.springframework.cloud.gateway.filter.factory.AddRequestHeaderGatewayFilterFactory;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfig {
+	@Bean
+	RouteLocator getGatewayRoute(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(route -> route
+						.path("/get")
+						.filters(f-> f
+								.addRequestHeader("Usuario", "Bruno Pasquetti")
+								.addRequestHeader("Teste", "ResponseHeader"))
+							.uri("http://httpbin.org"))
+				.route(route -> route
+						.path("/cambio-service/**")
+						.filters(f-> f
+								.addRequestHeader("Usuario", "Bruno Pasquetti"))
+						.uri("lb://cambio-service"))
+				.route(route -> route
+					.path("/produto-service/**")
+					.filters(f-> f
+							.addRequestHeader("Usuario", "Bruno Pasquetti"))
+					.uri("lb://produto-service"))
+				.route(route -> route
+						.path("/saudacao-service/**")
+						.filters(f-> f
+								.addRequestHeader("Usuario", "Bruno Pasquetti"))
+						.uri("lb://saudacao-service"))
+					.build();
+	}
+}
